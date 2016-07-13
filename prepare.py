74bd8998ceb6c84e51ec3d146b35119bd844639d
David J. H. Shih <djh.shih@gmail.com>
2016-07-13 16:26:03 -0400

    Fixed errors.

--- prepare.py
+++ prepare.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 
 pair_annot = pd.read_csv("pair_annotation.tsv", sep="\t")
-pair_annot.index = pair_annot["first_id"]
+pair_annot.index = pair_annot["case_id"]
 samples = list(pair_annot.index)
 
 sample_annot = pd.read_csv("sample_annotation.tsv", sep="\t")
@@ -15,19 +17,21 @@ def bam_path(sample_id):
     """Get bam file path of sample"""
     return sample_annot.loc[sample_id, "clean_bam_file_capture"]
 
-def second_id(first_id):
-    """Get the second sample of a pair"""
-    return pair_annot.loc[first_id, 'second_id']
+def control_id(case_id):
+    """Get the control sample of a case-control pair"""
+    return pair_annot.loc[case_id, 'control_id']
 
 def replace_fext(fname, ext):
     return fname[:-len(ext)] + ext
 
 
 import os, os.path
-os.makedirs('bam')
+root_dir = 'bam'
+if not os.path.exists(root_dir): os.makedirs(root_dir)
 for sample in set(pair_annot.stack()):
     source = sample_annot.loc[sample, "clean_bam_file_capture"]
-    target = "bam/{0}.bam".format(sample)
+    target = "{0}/{1}.bam".format(root_dir, sample)
+    print("{0} -> {1}".format(source, target))
     if not os.path.exists(target):
         os.symlink(source, target)
         os.symlink(replace_fext(source, "bai"), replace_fext(target, "bai"))
